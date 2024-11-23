db39baa2adfcde3b5f20e65fa6698f61a3ca0dcf
johnconell <johnconellcadorna@gmail.com>
2024-11-23 19:12:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import UserList from './UserList';
-import PrivateRoute from './PrivateRoute';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
-                <Route path="/users" element={
-                    <PrivateRoute>
-                        <UserList />
-                    </PrivateRoute>
-                } />
+                <Route path="/users" element={<UserList />} />
             </Routes>
         </Router>
     );
 }
 
 export default App;
-
